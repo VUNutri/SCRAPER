04800a9ca0d1e5dfdb723a78667815ce6f1292be
Dominykas Seputis <dom.seputis@gmail.com>
2018-12-05 00:17:56 +0200

    Scrapper fix

--- src/scraper.js
+++ src/scraper.js
@@ -4,27 +4,60 @@ import cheerio from 'cheerio';
 const getPrices = async (req, res) => {
   let url = 'https://www.barbora.lt/paieska?uzklausa=';
   const { products } = req.body;
+  const resProducts = [];
 
   await Promise.all(products.map(async (product) => {
     await axios.get(url.concat(product.title)).then(async (html) => {
       const $ = await cheerio.load(html.data);
-      const sizes = ['kg', 'g', 'l', 'ml'];
       const el = await $('.b-product-price-current-number');
       const elz = await $('.b-product-title > span');
-      const text = await $(elz[0]).text();
-      console.log(text);
-      for (let i in sizes) {
-        let idx = text.indexOf(i);
-        if (idx != -1) break;
+
+      let minPrice = 999999;
+      let minObj;
+
+      for (let i = 0; i < el.length; i++) {
+        const text = await $(elz[i]).text();
+        const price = await $(el[i]).text().replace(/\s/g, '').replace(/€/g, '').replace(/,/g, '.');
+        const resp = getParams(text, price, product);
+
+        if (resp.price < minPrice) {
+          minPrice = resp.price;
+          minObj = resp;
+        }
+        else console.log(resp);
       }
-
-      while(text[i])
-
-      const price = await $(el[0]).text().replace(/\s/g, '').replace(/€/g, '').replace(/,/g, '.');
-      product = await Object.assign(product, { price: parseFloat(price, 10) });
+      resProducts.push(minObj);
     });
   }));
-  return res.json(products);
+  return res.json(resProducts);
+};
+
+const getParams = (text, price, product) => {
+  const sizes = [' kg', ' ml', ' g', ' l'];
+  let size;
+  let idx;
+  for (let i in sizes) {
+    idx = text.indexOf(sizes[i]);
+    size = sizes[i].replace(/ /g, '');
+    if (idx != -1) break 
+  }
+  let a = text.slice(idx - 1, idx);
+  let c = idx;
+  while (a !== ' ') {
+    c -= 1;
+    a = text.slice(c - 1, c);
+  }
+  let ammount = parseFloat(text.slice(c, idx));
+  if (size === 'kg') {
+    size = 'g';
+    ammount = ammount * 1000;
+  } else if (size === 'l') {
+    size = 'ml';
+    ammount = ammount * 1000;
+  }
+  const vnt = Math.ceil(product.ammount / ammount);
+
+  return { description: text, ammount: vnt, price: price * vnt };
 };
 
 export default getPrices;
